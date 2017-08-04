2d068ea777cf8c712244c159e6b44ad8a2caaf75
bgok <ken@keepkey.com>
2017-08-03 20:44:30 -0700

    Fix cp command rename sh files

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo 'yea!'
+
 VERSION="0.14.6"
 FILENAME_ROOT="bitcoin-${VERSION}"
 FILENAME="${FILENAME_ROOT}-x86_64-linux-gnu.tar.gz"
@@ -8,7 +10,7 @@ DOWNLOAD_PATH="https://download.bitcoinabc.org/0.14.6/linux/${FILENAME}"
 curl -O ${DOWNLOAD_PATH}
 tar -xvzf ${FILENAME}
 
-cp -f ${FILENAME_ROOT} .
+cp -r ${FILENAME_ROOT}/* .
 
 rm ${FILENAME}
 rm -rf ${FILENAME_ROOT}
