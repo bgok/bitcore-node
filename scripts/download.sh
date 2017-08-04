817ae0195016991b5421fbcf5990eb38d32e27cc
bgok <ken@keepkey.com>
2017-08-04 09:43:24 -0700

    fix download.sh

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo 'yea!'
-
 VERSION="0.14.6"
 FILENAME_ROOT="bitcoin-${VERSION}"
 FILENAME="${FILENAME_ROOT}-x86_64-linux-gnu.tar.gz"
@@ -11,6 +9,8 @@ curl -O ${DOWNLOAD_PATH}
 tar -xvzf ${FILENAME}
 
 cp -r ${FILENAME_ROOT}/* .
+
+ln -sf "${FILENAME_ROOT}/bin/bitcoind"
 
 rm ${FILENAME}
 rm -rf ${FILENAME_ROOT}
