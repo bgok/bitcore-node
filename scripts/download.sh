4107d42644579db62ee4b0a126d94040fff3d8de
bgok <ken@keepkey.com>
2017-08-06 12:41:59 -0700

    use the installed bitcoind

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-VERSION="0.14.6"
-FILENAME_ROOT="bitcoin-${VERSION}"
-FILENAME="${FILENAME_ROOT}-x86_64-linux-gnu.tar.gz"
-DOWNLOAD_PATH="https://download.bitcoinabc.org/0.14.6/linux/${FILENAME}"
-
-curl -O ${DOWNLOAD_PATH}
-tar -xvzf ${FILENAME}
-
-cp -r ${FILENAME_ROOT}/* .
-
-ln -sf "${FILENAME_ROOT}/bin/bitcoind" /usr/bin/bitcoind
-
-rm ${FILENAME}
-rm -rf ${FILENAME_ROOT}
+ln -sf /usr/bin/bitcoind "${FILENAME_ROOT}/bin/bitcoind"
